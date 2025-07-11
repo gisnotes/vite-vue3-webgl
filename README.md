5577f72412e427c864b83e0432b76ed65bbd1c83
cxs <3528300287@qq.com>
2025-07-11 14:17:08 +0800

    利用矩阵缩放三角形

--- README.md
+++ README.md
@@ -19,3 +19,4 @@
 4. [平移三角形](https://github.com/gisnotes/vite-vue3-webgl/blob/main/src/views/chapter03/TranslateTriangle.vue)
 5. [旋转三角形](https://github.com/gisnotes/vite-vue3-webgl/blob/main/src/views/chapter03/RotatedTriangle.vue)
 6. [利用矩阵平移或旋转三角形](https://github.com/gisnotes/vite-vue3-webgl/blob/main/src/views/chapter03/TranslateRotatedTriangleMatrix.vue)
+7. [利用矩阵将三角形垂直方向(即y方向)上拉伸1.5倍](https://github.com/gisnotes/vite-vue3-webgl/blob/main/src/views/chapter03/ScaledTriangleMatrix.vue)
--- src/router/index.js
+++ src/router/index.js
@@ -20,8 +20,9 @@ const router = createRouter({
         // title: "绘制多边形",
         // title: "平移三角形",
         // title: "旋转三角形",
+        // title: "利用矩阵平移或旋转三角形",
         // #endregion
-        title: "利用矩阵平移或旋转三角形",
+        title: "利用矩阵将三角形垂直方向(即y方向)上拉伸1.5倍",
       },
       // #region 已完成
       // component: () => import("@/views/HelloWorld.vue"),
@@ -36,8 +37,9 @@ const router = createRouter({
       // component: () => import("@/views/chapter03/HelloQuad.vue"),
       // component: () => import("@/views/chapter03/TranslateTriangle.vue"),
       // component: () => import("@/views/chapter03/RotatedTriangle.vue"),
+      // component: () => import("@/views/chapter03/TranslateRotatedTriangleMatrix.vue"),
       // #endregion
-      component: () => import("@/views/chapter03/TranslateRotatedTriangleMatrix.vue"),
+      component: () => import("@/views/chapter03/ScaledTriangleMatrix.vue"),
     },
   ],
 });
